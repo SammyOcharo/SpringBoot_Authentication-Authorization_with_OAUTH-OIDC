3d1b94391ba93d3b6d007be5dfffa9dcd17b973f
sammy obanyi ocharo <obanyisammy22@gmail.com>
2024-05-29 07:26:11 +0300

    initialization project on spring boot Authentication and Authorization with OAUTH and IODC

---
+++ src/main/java/com/samdev/OAUTH_IODC/OauthIodcApplication.java
@@ -0,0 +1,13 @@
+package com.samdev.OAUTH_IODC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthIodcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthIodcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samdev/OAUTH_IODC/OauthIodcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samdev.OAUTH_IODC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthIodcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
